041c7c6155d76266a23bb08f5fedc3900ae88a7a
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-06 16:23:31 +0800

    Added Cookie Consent

--- InterrogateMe.Web/Startup.cs
+++ InterrogateMe.Web/Startup.cs
@@ -6,6 +6,7 @@ using InterrogateMe.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -63,6 +64,12 @@ namespace InterrogateMe.Web
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
