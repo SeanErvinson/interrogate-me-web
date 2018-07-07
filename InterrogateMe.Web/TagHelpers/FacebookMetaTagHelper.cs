846fc6eb28a209dd949de81948f69f14819650dc
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-07 16:41:19 +0800

    Added Twitter and Facebook Tag Helper for Metadata

---
+++ InterrogateMe.Web/TagHelpers/FacebookMetaTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace InterrogateMe.Web.TagHelpers
+{
+    public class FacebookMetaTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Type { get; set; }
+        public string ImageUrl { get; set; }
+        public string SiteName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SuppressOutput();
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"<meta property=\"og: url\" content=\"{Url}\">");
+            sb.AppendLine($"<meta property=\"og: type\" content=\"{Type}\">");
+            sb.AppendLine($"<meta property=\"og: title\" content=\"{Title}\">");
+            sb.AppendLine($"<meta property=\"og: site_name\" content=\"{SiteName}\">");
+            sb.AppendLine($"<meta property=\"og: description\" content=\"{Description}\">");
+            sb.AppendLine($"<meta property=\"og: image\" content=\"{ImageUrl}\">");
+
+            output.Content.SetHtmlContent(sb.ToString());
+        }
+    }
+}
---
+++ InterrogateMe.Web/TagHelpers/TwitterMetaTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace InterrogateMe.Web.TagHelpers
+{
+    public class TwitterMetaTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Site { get; set; }
+        public string Card { get; set; }
+        public string Creator { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SuppressOutput();
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"<meta name=\"twitter: card\" content=\"{Card}\">");
+            sb.AppendLine($"<meta name=\"twitter:site\" content=\"@{Site}\">");
+            sb.AppendLine($"<meta name=\"twitter:creator\" content=\"@{Creator}\">");
+            sb.AppendLine($"<meta name=\"twitter:title\" content=\"{Title}\">");
+            sb.AppendLine($"<meta name=\"twitter:description\" content=\"{Description}\">");
+            sb.AppendLine($"<meta name=\"twitter:image\" content=\"{ImageUrl}\">");
+
+            output.Content.SetHtmlContent(sb.ToString());
+        }
+    }
+}
