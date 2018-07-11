49c98c8b09ac9d1b27d420b6a2b9cc025785d2a8
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-11 13:47:43 +0800

    Change to implements base identifier

--- InterrogateMe.Core/Models/IpAddress.cs
+++ InterrogateMe.Core/Models/IpAddress.cs
@@ -1,18 +1,11 @@
-﻿using System;
+﻿using InterrogateMe.Core.Models.Base;
 using System.ComponentModel.DataAnnotations;
 
 namespace InterrogateMe.Core.Models
 {
-    public class IpAddress : BaseModel
+    public class IpAddress : BaseIdentifier
     {
         [Required]
         public string Address { get; set; }
-
-        public string UserAgent { get; set; }
-
-        [Required]
-        public Guid TopicId { get; set; }
-
-        public Topic Topic { get; set; }
     }
 }
