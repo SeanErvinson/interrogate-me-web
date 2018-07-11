7b8bb729e4832c15a2cd5ecd4f38afd32ff8354b
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-11 14:27:54 +0800

    Added Duplication Checking

---
+++ InterrogateMe.Core/Models/DuplicationCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterrogateMe.Core.Models
+{
+    public enum DuplicationCheck
+    {
+        [Display(Name = "No Duplication Checking")]
+        None = 0,
+        [Display(Name = "Ip Duplication Checking")]
+        IpCheck = 1 << 0,
+        [Display(Name = "Browser Cookie Duplication Checking")]
+        CookieCheck = 1 << 1
+    }
+}
--- InterrogateMe.Core/Models/Topic.cs
+++ InterrogateMe.Core/Models/Topic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using InterrogateMe.Core.Models.Base;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace InterrogateMe.Core.Models
@@ -12,11 +13,14 @@ namespace InterrogateMe.Core.Models
         [Display(Name = "Allow Multiple Likes")]
         public bool AllowMultipleLikes { get; set; }
 
-        [Display(Name = "Prevent Ip Duplication")]
-        public bool PreventIpDuplication { get; set; }
+        [Display(Name = "Improve Spam Prevention")]
+        public bool PreventSpam { get; set; }
 
         [Display(Name ="Prevent NSFW")]
         public bool PreventNSFW { get; set; }
+
+        [EnumDataType(typeof(DuplicationCheck))]
+        public DuplicationCheck DuplicationCheck { get; set; }
 
         public ICollection<Question> Questions { get; set; }
     }
