900a6faf3d7c0c28a309a18a74224b2127bfd8f3
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-11 13:47:21 +0800

    Added browser cookie to track cookies

---
+++ InterrogateMe.Core/Models/BrowserCookie.cs
@@ -0,0 +1,11 @@
+﻿using InterrogateMe.Core.Models.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace InterrogateMe.Core.Models
+{
+    public class BrowserCookie : BaseIdentifier
+    {
+        [Required]
+        public string Cookie { get; set; }
+    }
+}
