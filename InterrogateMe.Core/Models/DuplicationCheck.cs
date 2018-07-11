bb5dd4118a0741649b367b952f809b5f7273d726
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-11 19:31:13 +0800

    Renamed the properties

--- InterrogateMe.Core/Models/DuplicationCheck.cs
+++ InterrogateMe.Core/Models/DuplicationCheck.cs
@@ -7,8 +7,8 @@ namespace InterrogateMe.Core.Models
         [Display(Name = "No Duplication Checking")]
         None = 0,
         [Display(Name = "Ip Duplication Checking")]
-        IpCheck = 1 << 0,
+        IpAddress = 1 << 0,
         [Display(Name = "Browser Cookie Duplication Checking")]
-        CookieCheck = 1 << 1
+        BrowserCookie = 1 << 1
     }
 }
