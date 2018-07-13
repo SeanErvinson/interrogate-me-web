ad9a272c676eb064284ce195a18ae14e414ca36f
Sean Ervinson Ong <ervinsonong@gmail.com>
2018-07-14 01:27:53 +0800

    Added more fallback, recaptcha, duplication check, error message

--- InterrogateMe.Web/Pages/Question.cshtml.cs
+++ InterrogateMe.Web/Pages/Question.cshtml.cs
@@ -5,9 +5,11 @@ using InterrogateMe.Utilities;
 using InterrogateMe.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace InterrogateMe.Web.Pages
 {
@@ -15,25 +17,28 @@ namespace InterrogateMe.Web.Pages
     {
         #region Private Variable
 
-        private readonly IRepository _repository;
         private readonly ILogger _logger;
+        private readonly IRepository _repository;
+        private readonly IConfiguration _configuration;
 
         #endregion Private Variable
 
         #region Public Properties
 
         [BindProperty]
-        public string TempLink { get; set; }
-
-        [BindProperty]
         public Question Question { get; set; }
 
-        [TempData]
-        public bool IsValidQuestion { get; set; } = true;
+        [BindProperty]
+        public string ShortcutLink { get; set; }
+
+        [BindProperty]
+        public string Title { get; set; }
+
+        [BindProperty]
+        public bool ShowReCaptcha { get; set; }
 
         public InterrogateClient InterrogateClient { get; }
-        public string Title { get; set; }
-        public string ShortcutLink { get; set; }
+
 
         #endregion Public Properties
 
@@ -41,28 +46,46 @@ namespace InterrogateMe.Web.Pages
         /// Candidate for refactoring. Implementing a IService or Service class in the middle
         /// that is triggered by an event
         /// </summary>
-        public QuestionModel(IRepository repository, ILogger<QuestionModel> logger, InterrogateClient interrrogateClient)
+        public QuestionModel(IRepository repository, ILogger<QuestionModel> logger, IConfiguration configuration, InterrogateClient interrrogateClient)
         {
             _repository = repository;
             _logger = logger;
-            this.InterrogateClient = interrrogateClient;
+            _configuration = configuration;
+            InterrogateClient = interrrogateClient;
         }
 
-        public void OnGet(string link)
+        public IActionResult OnGet(string link)
         {
+            if (link == null)
+            {
+                return NotFound();
+            }
+
             var resultLink = _repository.Single(LinkSpecification.ByUrl(link));
+            if (resultLink == null)
+            {
+                return NotFound();
+            }
+
             var resultTopic = _repository.Single(BaseSpecification<Topic>.ById(resultLink.TopicId));
+            if (resultTopic == null)
+            {
+                return NotFound();
+            }
+
+            if (resultTopic.PreventSpam)
+            {
+                ShowReCaptcha = true;
+            }
 
             Title = resultTopic.Title;
             ShortcutLink = link;
+            return Page();
         }
 
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
-                return Page();
-
-            var resultLink = _repository.Single(LinkSpecification.ByUrl(TempLink));
+            var resultLink = _repository.Single(LinkSpecification.ByUrl(ShortcutLink));
 
             if (resultLink == null)
                 return NotFound();
@@ -72,66 +95,64 @@ namespace InterrogateMe.Web.Pages
             if (resultTopic == null)
                 return NotFound();
 
-            if (resultTopic.DuplicationCheck == DuplicationCheck.IpAddress)
-            {
-                if (IsDuplicateIp(WebHelper.GetRemoteIP))
-                    return RedirectToPage();
-
-                AddIpAddress(resultTopic.Id);
-            }
-
-            if (resultTopic.DuplicationCheck == DuplicationCheck.BrowserCookie)
-            {
-                const string cookieKey = "PID";
-                if (CookieExists(cookieKey, TempLink))
-                    return RedirectToPage();
-                WebHelper.SetCookie(cookieKey, TempLink, null);
-            }
-
             if (resultTopic.PreventNSFW)
             {
-                if (!IsNsfw(Question.Content))
-                {
-                    IsValidQuestion = false;
-                    return RedirectToPage();
-                }
+                if (IsNsfw(Question.Content))
+                    ModelState.AddModelError("Question.Content", "Question contains word/s that are considered as NSFW");
             }
+
+            if (resultTopic.PreventSpam)
+            {
+                if(!ReCaptchaHelper.ValidateRecaptcha(Request.Form["g-recaptcha-response"]))
+                    ModelState.AddModelError("Question.Content", "Invalid ReCaptcha");
+            }
+
+            switch (resultTopic.DuplicationCheck)
+            {
+                case DuplicationCheck.IpAddress:
+                    {
+                        if (IpAddressExists(WebHelper.GetRemoteIP, resultTopic.Id))
+                        {
+                            ModelState.AddModelError("Question.Content", "You have already asked a question on this poll");
+                            break;
+                        }
+                        else if (ModelState.IsValid)
+                            AddIpAddress(resultTopic.Id);
+                        break;
+                    }
+                case DuplicationCheck.BrowserCookie:
+                    {
+                        const string cookieKey = "PID";
+                        if (CookieExists(cookieKey, ShortcutLink))
+                        {
+                            ModelState.AddModelError("Question.Content", "You have already asked a question on this poll");
+                            break;
+                        }
+                        else if(ModelState.IsValid)
+                            WebHelper.SetCookie(cookieKey, ShortcutLink, null);
+                        break;
+                    }
+                case DuplicationCheck.None: break;
+                default:
+                    _logger.LogError($"Unkown duplication check case {resultTopic.DuplicationCheck}.");
+                    throw new InvalidEnumArgumentException("Unrecognized case value.");
+            }
+
+            if (!ModelState.IsValid)
+                return Page();
 
             resultTopic.Questions.Add(Question);
 
             _repository.Update(resultTopic);
 
-            InterrogateClient.UpdateList(TempLink, Question);
+            InterrogateClient.UpdateList(ShortcutLink, Question);
 
-            InterrogateClient.UpdateParticipantCount(TempLink, resultTopic.Questions.Count);
+            InterrogateClient.UpdateQuestionCount(ShortcutLink, resultTopic.Questions.Count);
 
             return RedirectToPage("Result");
         }
 
-        private void AddIpAddress(Guid id)
-        {
-            _repository.Add(new IpAddress
-            {
-                Address = WebHelper.GetRemoteIP,
-                UserAgent = WebHelper.GetUserAgent,
-                RequestScheme = WebHelper.GetScheme,
-                TopicId = id
-            });
-        }
-
-        private bool IsNsfw(string question)
-        {
-            const string pattern = @"\s+";
-            var words = System.Text.RegularExpressions.Regex.Split(question.ToLower().Trim(), pattern);
-            foreach (var word in words)
-            {
-                if (_repository.All(ProfaneWordSpecification.ByWord(word)).Count > 0)
-                    return false;
-            }
-            return true;
-        }
-
-        private bool CookieExists(string key, string value)
+        private static bool CookieExists(string key, string value)
         {
             var cookieValue = WebHelper.GetCookie(key);
             if (cookieValue == null)
@@ -145,9 +166,32 @@ namespace InterrogateMe.Web.Pages
             return false;
         }
 
-        private bool IsDuplicateIp(string ipAddress)
+        private void AddIpAddress(Guid id)
         {
-            return _repository.Single(IpAddressSpecification.ByIpAddress(ipAddress)) != null;
+            _repository.Add(new IpAddress
+            {
+                Address = WebHelper.GetRemoteIP,
+                UserAgent = WebHelper.GetUserAgent,
+                RequestScheme = WebHelper.GetScheme,
+                TopicId = id
+            });
+        }
+
+        private bool IpAddressExists(string ipAddress, Guid topicId)
+        {
+            return _repository.Single(IpAddressSpecification.ByIpAddressAndTopicId(ipAddress, topicId)) != null;
+        }
+
+        private bool IsNsfw(string question)
+        {
+            const string pattern = @"\s+";
+            var words = System.Text.RegularExpressions.Regex.Split(question.ToLower().Trim(), pattern);
+            foreach (var word in words)
+            {
+                if (_repository.All(ProfaneWordSpecification.ByWord(word)).Count > 0)
+                    return true;
+            }
+            return false;
         }
     }
 }
