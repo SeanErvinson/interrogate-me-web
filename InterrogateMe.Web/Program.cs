baa37735db3a8a96b3be48b80af191cbe8389c43
Sean Ervinson <ervinsonong@gmail.com>
2020-04-04 13:35:54 +0800

    Updated to 3.1 aspnet core

--- InterrogateMe.Web/Program.cs
+++ InterrogateMe.Web/Program.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore;
+﻿
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 namespace InterrogateMe.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConsole();
+                    })
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
     }
 }
+
--- InterrogateMe.Web/Startup.cs
+++ InterrogateMe.Web/Startup.cs
@@ -7,10 +7,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace InterrogateMe.Web
@@ -24,30 +24,32 @@ namespace InterrogateMe.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddEntityFrameworkNpgsql().AddDbContext<InterrogateDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("InterrogateDbContext")));
 
+            services.AddControllersWithViews();
+
+            services.AddRazorPages();
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddScoped<IRepository, Repository>();
 
             services.AddSingleton<InterrogateClient>();
-                        
-            services.AddSignalR(hubOptions => 
+
+            services.AddSignalR(hubOptions =>
             {
                 hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(5);
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            services.AddMvc()
                 .AddRazorPagesOptions(options =>
                 {
                     options.Conventions.AddPageRoute("/Question", "/{link}");
@@ -57,7 +59,7 @@ namespace InterrogateMe.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
             InitializeDatabase(app);
@@ -78,26 +80,16 @@ namespace InterrogateMe.Web
 
             app.UseHttpsRedirection();
 
-            app.UseSignalR(route =>
-            {
-                route.MapHub<InterrogateHub>("/interrogate");
-            });
+            app.UseRouting();
 
-            app.UseStaticFiles(new StaticFileOptions
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
             {
-                OnPrepareResponse = ctx =>
-                {
-                    var path = ctx.File.PhysicalPath;
-                    if (path.EndsWith(".css") || path.EndsWith(".js") || path.EndsWith(".gif") || path.EndsWith(".jpg") || path.EndsWith(".png") || path.EndsWith(".svg"))
-                    {
-                        var maxAge = new TimeSpan(7, 0, 0, 0);
-                        ctx.Context.Response.Headers.Append("Cache-Control", "max-age=" + maxAge.TotalSeconds.ToString("0"));
-                    }
-                }
+                endpoints.MapHub<InterrogateHub>("/interrogate");
+                endpoints.MapRazorPages();
             });
             app.UseCookiePolicy();
-
-            app.UseMvc();
 
             WebHelper.Configure(app.ApplicationServices.GetRequiredService<IHttpContextAccessor>());
             ReCaptchaHelper.Configure(app.ApplicationServices.GetRequiredService<IConfiguration>());
